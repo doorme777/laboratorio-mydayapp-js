4f6c30e891633138aa1e66277224141d41272452
doorme777 <137806586+doorme777@users.noreply.github.com>
2024-05-17 14:11:45 -0600

    Se resolvio el primer test de que si no hay task no hay main, o footer.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import "./css/base.css";
 
-import { sayHello } from "./js/utils";
+import { GeneratorTask } from "./js/utils.mjs";
 
-console.log(sayHello("Hello"));
+export const generatorTask = new GeneratorTask();
+
+export const main = document.querySelector(".main");
+export const footer = document.querySelector(".footer");
+
+window.addEventListener("load", function () {
+  console.log("La página ha cargado!");
+  if (generatorTask.getTasks().length === 0) {
+    main.style.display = "none";
+    footer.style.display = "none";
+    return 0;
+  }
+  main.style.display = "block";
+  footer.style.display = "block";
+});
+
+window.addEventListener("hashchange", function () {
+  console.log("La URL ha cambiado!");
+});
